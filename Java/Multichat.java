4c89537c4b69a09a5e9665ee4b6cf4df36bd41f8
palakgarg2107 <sahil.070197@gmail.com>
2017-07-11 17:42:07 +0530

    Java multithreaded multiclient chat application

---
+++ Java/Multichat.java
@@ -0,0 +1,161 @@
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Shared
+{
+    static ServerSocket server;
+}
+
+class MyAllSockets
+{
+    static ArrayList<Socket> arr=new ArrayList<>();
+    static ArrayList<PrintWriter> parr=new ArrayList<>();
+    static Map<String, Integer> Kmap=new HashMap<>();
+}
+
+class ReadThread extends Thread
+{
+    int index;
+    String name;
+    public ReadThread(int index) 
+    {
+        this.index=index;
+        name=null;
+    }
+    
+    public void run()
+    {
+        try 
+        {
+            int flag=0;
+            BufferedReader brs=new BufferedReader(new InputStreamReader(MyAllSockets.arr.get(index).getInputStream()));
+            while(true)
+            {
+                if(flag==0)
+                {
+                    flag=1;
+                    String s=getClientName(brs);
+                    MyAllSockets.Kmap.put(s, index);
+                    this.name=s;
+                    echoOnlineClients();
+                }
+                else
+                {
+                    normalReader(brs);
+                }
+            }
+        } 
+        catch (Exception ex) 
+        {
+            Logger.getLogger(ReadThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    String getClientName(BufferedReader brs) throws Exception
+    {
+        String name=brs.readLine();
+        System.out.println(name+" has connected.");
+        return name;
+    }
+    void echoOnlineClients() throws Exception
+    {
+        
+        Object[] ar=MyAllSockets.Kmap.keySet().toArray();
+        PrintWriter pw=MyAllSockets.parr.get(index);
+        System.out.print("index "+index);
+        pw.println( "Hello zxhjnkj ");
+        StringBuffer buf=new StringBuffer();
+        for (Object clientName : ar)
+        {
+            System.out.println("Online: "+clientName.toString());
+            buf.append(clientName.toString()+"\n");
+        }
+        pw.println(buf.toString());
+    }
+    void normalReader(BufferedReader reader) throws Exception
+    {
+        String s=reader.readLine();
+        if(s==null)
+        {
+            throw new RuntimeException();
+        }
+        System.out.println(name+": "+s);
+    }
+}
+
+class MyClientConnection extends Thread
+{
+    PrintWriter pw;
+    Socket socket;
+    public void run()
+    {
+        while(true)
+        {
+            try 
+            {
+                socket=Shared.server.accept();
+                init();
+                new ReadThread(MyAllSockets.parr.size()-1).start();
+            } 
+            catch (Exception ex) 
+            {
+                System.exit(0);
+                break;
+            }
+        }
+    }
+    void init() throws Exception
+    {
+        pw=new PrintWriter(socket.getOutputStream(), true);
+        MyAllSockets.parr.add(pw);
+        MyAllSockets.arr.add(socket);
+    }
+}
+
+public class Multichat 
+{
+    public static final int portNumber=3032;
+    static BufferedReader br;
+    static void init() throws Exception
+    {
+        br=new BufferedReader(new InputStreamReader(System.in));
+        Shared.server=new ServerSocket(3032);
+        MyClientConnection mcc=new MyClientConnection();
+        mcc.start();
+    }
+
+    public static void main(String[] args) 
+    {
+        try
+        {
+    
+            System.out.println("Server is going to run...");
+            init();   
+            System.out.println("Server is running at IP: "+InetAddress.getLocalHost());
+    
+            while(true)
+            {
+                broadcast();
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Exception caught: "+ex);
+        }
+    }
+    static void broadcast() throws Exception
+    {
+        System.out.println("Enter text to broadcast: ");
+        String str=br.readLine();
+        for(int i=0;i<MyAllSockets.parr.size();i++)
+        {
+            MyAllSockets.parr.get(i).println(str);
+        }
+    }
+}
